3c1c36941c32ab27897073449a4eda411281b8d1
trumpowen <okessuman@st.ug.edu.gh>
2022-01-16 16:59:36 -0500

    Removed celery to enhance deployment on heroku becuase redis service is paid

--- focus/focus/settings.py
+++ focus/focus/settings.py
@@ -97,9 +97,3 @@ STATIC_URL = 'static/'
 UPLOADS_DIR = 'uploads/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_ACCEPT_CONTENT = ['application/json']
